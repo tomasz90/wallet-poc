a7a0fbc9749a9c9331738848e93c225daa0da9af
tomasz <tomaszkacperkulig@gmail.com>
2023-05-27 00:57:40 +0200

    working nicely

--- src/util/menu.cpp
+++ src/util/menu.cpp
@@ -16,28 +16,20 @@ void Menu::begin() {
     State *S2_0 = machine.addState(&s2_0);
     State *S2_1 = machine.addState(&s2_1);
     State *S3 = machine.addState(&s3);
-    State *S4 = machine.addState(&s4);
-    State *S5 = machine.addState(&s5);
-
 
     // TRANSITIONS
     S0->addTransition(&next, S1);
     S1->addTransition(&next, S2_0);
     S2_0->addTransition(&nextSubChoice, S2_1);
     S2_1->addTransition(&nextSubChoice, S2_0);
-    S3->addTransition(&next, S4);
 
-    S4->addTransition(&previous, S3);
     S3->addTransition(&previous, S2_1);
     S2_1->addTransition(&previousSubChoice, S2_0);
     S2_0->addTransition(&previousSubChoice, S2_1);
     S1->addTransition(&previous, S0);
-    S0->addTransition(&previous, S4);
 
     S2_0->addTransition(&both, S1);
     S2_1->addTransition(&both, S3);
-
-    S5->addTransition(&both, S0);
 }
 
 bool Menu::next() {
@@ -101,14 +93,6 @@ void Menu::s2_1() {
 }
 
 void Menu::s3() {
-    Display::blinkTextWithSign("s3");
-}
-
-void Menu::s4() {
-    Display::blinkTextWithSign("Enter your seed?");
-}
-
-void Menu::s5() {
-    Display::blinkTextWithSign("confirmed");
+    Display::blinkTextWithSign("Great!");
 }
 
