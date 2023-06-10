86a63a1e3315c06213e16ffc8db468d23043937b
tomasz <tomaszkacperkulig@gmail.com>
2023-06-10 21:03:39 +0200

    fixed

--- src/util/SeedGenerator.cpp
+++ src/util/SeedGenerator.cpp
@@ -1,34 +1,35 @@
 #include <bootloader_random.h>
 #include "SeedGenerator.h"
 
+#define DEFAULT_PATH "m/44'/60'/0'/0/0"
+
 SeedGenerator::SeedGenerator(DataHolder *dataHolder) {
     // GENERATE RANDOMNESS
     bootloader_random_enable();
-    vector<uint8_t> entropy = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};//generateEntropy();
+    vector<uint8_t> entropy = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};//generateEntropy();
     generateRandomSequence(dataHolder);
     bootloader_random_disable();
 
     dataHolder->mnemonic = create_mnemonic(entropy, language::en);
     EthereumHDPrivateKey hd(dataHolder->mnemonic.to_string());
-    dataHolder->account = hd.derive("m/44'/60'/0'/0/0");
+    dataHolder->account = hd.derive(DEFAULT_PATH);
     Serial.println(dataHolder->mnemonic.to_string().c_str());
 }
 
 void SeedGenerator::generateRandomSequence(DataHolder *dataHolder) {
-    string s;
-    array<int, MNEMONIC_LENGTH> randomSequence = dataHolder->randomSequence;
-    randomSequence.fill(-1);
-    for (int &i: randomSequence) {
+    array<int, MNEMONIC_LENGTH> seq{};
+    seq.fill(-1);
+    for (int &i: seq) {
         uint8_t temp = esp_random() % MNEMONIC_LENGTH;
-        while (std::find(randomSequence.begin(), randomSequence.end(), temp) != randomSequence.end()) {
+        while (find(seq.begin(), seq.end(), temp) != seq.end()) {
             temp = esp_random() % MNEMONIC_LENGTH;
         }
         i = temp;
     }
+    dataHolder->randomSequence = seq;
 }
 
 vector<uint8_t> SeedGenerator::generateEntropy() {
-
     // Resize the vector to accommodate the desired number of bytes
     size_t numBytes = 16;
     vector<uint8_t> entropy(numBytes);
