668622cbea0025c7bae7b2f1343ef213acd7b4da
tomasz <tomaszkacperkulig@gmail.com>
2023-06-15 17:46:36 +0200

    reverted web3e

--- .pio/libdeps/esp32dev/Web3E/src/Trezor/hmac.h
+++ .pio/libdeps/esp32dev/Web3E/src/Trezor/hmac.h
@@ -40,7 +40,7 @@ typedef struct _HMAC_SHA512_CTX {
 void hmac_sha256_Init(HMAC_SHA256_CTX *hctx, const uint8_t *key, const uint32_t keylen);
 void hmac_sha256_Update(HMAC_SHA256_CTX *hctx, const uint8_t *msg, const uint32_t msglen);
 void hmac_sha256_Final(HMAC_SHA256_CTX *hctx, uint8_t *hmac);
-void hmac_sha256(const uint8_t *key, const uint32_t keylen, const uint8_t *msg, const uint32_t msglen, uint8_t *hmac);
+void _hmac_sha256(const uint8_t *key, const uint32_t keylen, const uint8_t *msg, const uint32_t msglen, uint8_t *hmac);
 void hmac_sha256_prepare(const uint8_t *key, const uint32_t keylen, uint32_t *opad_digest, uint32_t *ipad_digest);
 
 void hmac_sha512_Init(HMAC_SHA512_CTX *hctx, const uint8_t *key, const uint32_t keylen);
